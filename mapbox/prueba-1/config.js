9b5a6275fdb45178589df3fcdc484bb35c2adc2a
Mariolv <mchaparro@lavanguardia.es>
2021-06-20 14:36:19 +0200

    Update config.js

--- mapbox/prueba-1/config.js
+++ mapbox/prueba-1/config.js
@@ -1,20 +1,20 @@
 var config = {
-    style: 'mapbox://styles/conisco/ck2s7cs0g1ank1cqowi649zul',
+    style: 'mapbox://styles/conisco/ckowq6z8m0tgq18pchl47cf5b',
     accessToken: 'pk.eyJ1IjoiY29uaXNjbyIsImEiOiJjamxkaWNyOXUwYW4zM3ZxZ2c1ZXFlNWE2In0.ytG8rMF1nf5OZtOMpZVQHg',
     showMarkers: false,
     theme: 'light',
-    use3dTerrain: true,
-    title: '',
-    subtitle: '',
+    use3dTerrain: false,
+    title: 'Prueba de StoryMap de Mapbox',
+    subtitle: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
     byline: '',
-    footer: '',
+    footer: 'Fuente: Lorem ipsum dolor sit amet consectetur adipiscing elit. Datos: <a href="https://es.wikipedia.org">Wikipedia</a>',
     chapters: [
         {
-            id: 'intro-prueba',
+            id: 'inicio',
             alignment: 'left',
-            title: 'Glacier National Park Glaciers',
+            title: 'Inicio',
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg/800px-2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg',
-            description: 'Glacier National Park is dominated by mountains which were carved into their present shapes by the huge glaciers of the last ice age. These glaciers have largely disappeared over the last 12,000 years. Evidence of widespread glacial action is found throughout the park in the form of U-shaped valleys, cirques, arêtes, and large outflow lakes radiating like fingers from the base of the highest peaks. Since the end of the ice ages, various warming and cooling trends have occurred. The last recent cooling trend was during the Little Ice Age, which took place approximately between 1550 and 1850. During the Little Ice Age, the glaciers in the park expanded and advanced, although to nowhere near as great an extent as they had during the Ice Age.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem. Etiam hendrerit libero vel neque fringilla, vulputate mollis nisl ullamcorper. Praesent ac massa nec velit fringilla viverra ut quis felis.',
             location: {
                 center: [-113.91666, 48.66451],
                 zoom: 1.50,
@@ -23,8 +23,8 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'new-york',
-                    opacity: 0.9
+                    layer: 'gnpglaciers-1998',
+                    opacity: 0.25
                 },
                 {
                     layer: 'glaciernp-boundary',
@@ -39,40 +39,30 @@ var config = {
             ]
         },
         {
-            id: 'new-york',
-            alignment: 'left',
-            title: 'New York',
-            image: '',
-            description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit festinamus urb et orbe sic transit gloria mundi.',
-            location: {
-                center: [-73.97843, 40.73542],
-                zoom: 6.75,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-        },
-        {
-            id: 'harrison2015',
+            id: 'barcelona',
             alignment: 'left',
             title: 'Barcelona',
             image: '',
-            description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit festinamus urb et orbe sic transit gloria mundi.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem.',
             location: {
-                center: [2.19055, 41.38305],
+                center: [2.17459, 41.38111],
                 zoom: 7.68,
-                pitch: 0.00,
+                pitch: 50.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'madrid',
+            alignment: 'left',
+            title: 'Madrid',
+            image: '',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem.',
+            location: {
+                center: [-3.69422, 40.41576],
+                zoom: 8.46,
+                pitch: 40.00,
                 bearing: 0.00
             },
             onChapterEnter: [
@@ -89,70 +79,15 @@ var config = {
             ]
         },
         {
-            id: 'blackfoot1998',
+            id: 'paris',
             alignment: 'left',
-            title: 'Blackfoot Glacier, 1998',
+            title: 'París',
             image: '',
-            description: 'Blackfoot Glacier is the second largest of the remaining 25 glaciers in Glacier National Park, Montana. Blackfoot Glacier is just to the north of Blackfoot Mountain and near Jackson Glacier. When first documented in 1850, the glacier also included the now separate Jackson Glacier and together, they covered 1,875 acres (7.59 km2). In 1850, there were an estimated 150 glaciers in the park. Glaciologists have stated that by the year 2030, all the glaciers in the park may disappear.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem.',
             location: {
-                center: [-113.66573, 48.59181],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'blackfoot2015',
-            alignment: 'left',
-            title: 'Blackfoot Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Blackfoot Glacier lost 31 acres of surface area (about 8%).',
-            location: {
-                center: [-113.66573, 48.59181],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-        },
-        {
-            id: 'agassiz1998',
-            alignment: 'left',
-            title: 'Agassiz Glacier, 1998',
-            image: '',
-            description: 'Agassiz Glacier is in Glacier National Park in the U.S. state of Montana. It is named after Louis Agassiz, a Swiss-American glaciologist. The glacier is situated in a cirque to the southeast of Kintla Peak west of the Continental Divide. Agassiz Glacier is one of several glaciers that have been selected for monitoring by the U.S. Geological Survey\'s Glacier Monitoring Research program, which is researching changes to the mass balance of glaciers in and surrounding Glacier National Park.',
-            location: {
-                center: [-114.15881, 48.93439],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'agassiz2015',
-            alignment: 'left',
-            title: 'Agassiz Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Agassiz Glacier lost 108 acres of surface area (about 37%).',
-            location: {
-                center: [-114.15881, 48.93439],
-                zoom: 1.50,
-                pitch: 0.00,
+                center: [2.34220, 48.86520],
+                zoom: 8.27,
+                pitch: 47.00,
                 bearing: 0.00
             },
             onChapterEnter: [
@@ -169,31 +104,16 @@ var config = {
             ]
         },
         {
-            id: 'rainbow1998',
+            id: 'dublín',
             alignment: 'left',
-            title: 'Rainbow Glacier, 1998',
+            title: 'Dublín',
             image: '',
-            description: 'Rainbow Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated immediately to the east of Rainbow Peak at an elevation between 8,500 feet (2,600 m) and 8,000 feet (2,400 m) above sea level. The glacier has visible crevasses in satellite imagery. Rainbow Glacier has shown modest retreat compared to other glaciers in Glacier National Park.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem.',
             location: {
-                center: [-114.08659, 48.88039],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rainbow2015',
-            alignment: 'left',
-            title: 'Rainbow Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Rainbow Glacier lost 17 acres of surface area (about 6%).',
-            location: {
-                center: [-114.08659, 48.88039],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
+                center: [-6.25841, 53.35098],
+                zoom: 9.87,
+                pitch: 35.50,
+                bearing: -12.80
             },
             onChapterEnter: [
                 {
@@ -209,28 +129,28 @@ var config = {
             ]
         },
         {
-            id: 'kintla1998',
+            id: 'nueva york',
             alignment: 'left',
-            title: 'Kintla Glacier, 1998',
+            title: 'Nueva York',
             image: '',
-            description: 'Kintla Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated on a plateau 2 miles (3.2 km) southwest of Kintla Peak at an elevation between 8,700 feet (2,700 m) and 7,700 feet (2,300 m) above sea level. The glacier has numerous crevasses and is actually two glaciers.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem.',
             location: {
-                center: [-114.18755, 48.92880],
-                zoom: 1.50,
+                center: [-73.98784, 40.75006],
+                zoom: 10.77,
                 pitch: 0.00,
-                bearing: 0.00
+                bearing: -2.59
             },
             onChapterEnter: [],
             onChapterExit: []
         },
         {
-            id: 'kintla2015',
+            id: 'final',
             alignment: 'left',
-            title: 'Kintla Glacier, 2015',
+            title: 'Final',
             image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 24 acres of surface area (about 10%).',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget venenatis orci, a sollicitudin nulla. Proin sit amet pharetra nulla, vitae cursus nisl. Praesent quam mi, lacinia vitae quam eu, ornare pulvinar urna. Sed aliquet turpis consectetur, tristique risus eu, molestie ante. Donec dapibus tincidunt ligula, et cursus nunc. Mauris scelerisque, nibh ut sollicitudin aliquet, libero enim congue erat, non sagittis tellus magna et sem.).',
             location: {
-                center: [-114.18755, 48.92880],
+                center: [-113.91666, 48.66451],
                 zoom: 1.50,
                 pitch: 0.00,
                 bearing: 0.00
@@ -238,7 +158,7 @@ var config = {
             onChapterEnter: [
                 {
                     layer: 'gnpglaciers-2015',
-                    opacity: 0.25
+                    opacity: 0.25,
                 }
             ],
             onChapterExit: [
@@ -247,46 +167,6 @@ var config = {
                     opacity: 0
                 }
             ]
-        },
-        {
-            id: 'sperry1998',
-            alignment: 'left',
-            title: 'Sperry Glacier, 1998',
-            image: '',
-            description: 'Sperry Glacier is a glacier on the north slopes of Gunsight Mountain west of the Continental Divide in Glacier National Park in the U.S. state of Montana. Although many geologic features of Glacier National Park were formed during the much longer period of glaciation ending over 10,000 years ago, Sperry Glacier — like all the glaciers in the park today — is a product of the recent Little Ice Age, the period of cooler average temperatures starting in about the 13th century and concluding in the mid-19th century.',
-            location: {
-                center: [-113.75672, 48.62433],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'sperry2015',
-            alignment: 'left',
-            title: 'Sperry Glacier, 2015',
-            image: '',
-            description: 'Between 1998 and 2015, Harrison Glacier lost 37 acres of surface area (about 16%).',
-            location: {
-                center: [-113.75672, 48.62433],
-                zoom: 1.50,
-                pitch: 0.00,
-                bearing: 0.00
-            },
-            onChapterEnter: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0.25
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'gnpglaciers-2015',
-                    opacity: 0
-                }
-            ]
-         }
+        }
     ]
 };
